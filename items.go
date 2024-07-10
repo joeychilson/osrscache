e8fe38ff6180c0a862b7f5c8c5a362d4b8619e00
Joey Chilson <joeychilson@outlook.com>
2024-07-09 19:12:19 -0500

    clean up items

--- items.go
+++ items.go
@@ -111,105 +111,85 @@ func (def *ItemDefinition) Read(data []byte) error {
 		}
 		switch opcode {
 		case 1:
-			def.InventoryModelData.ID, err = reader.ReadUint16()
-			if err != nil {
+			if def.InventoryModelData.ID, err = reader.ReadUint16(); err != nil {
 				return fmt.Errorf("reading inventory model id: %w", err)
 			}
 		case 2:
-			def.Name, err = reader.ReadString()
-			if err != nil {
+			if def.Name, err = reader.ReadString(); err != nil {
 				return fmt.Errorf("reading name: %w", err)
 			}
 		case 3:
-			def.Examine, err = reader.ReadString()
-			if err != nil {
+			if def.Examine, err = reader.ReadString(); err != nil {
 				return fmt.Errorf("reading examine: %w", err)
 			}
 		case 4:
-			def.InventoryModelData.Zoom, err = reader.ReadUint16()
-			if err != nil {
+			if def.InventoryModelData.Zoom, err = reader.ReadUint16(); err != nil {
 				return fmt.Errorf("reading inventory model zoom2d: %w", err)
 			}
 		case 5:
-			def.InventoryModelData.RotationX, err = reader.ReadUint16()
-			if err != nil {
+			if def.InventoryModelData.RotationX, err = reader.ReadUint16(); err != nil {
 				return fmt.Errorf("reading inventory model xan2d: %w", err)
 			}
 		case 6:
-			def.InventoryModelData.RotationY, err = reader.ReadUint16()
-			if err != nil {
+			if def.InventoryModelData.RotationY, err = reader.ReadUint16(); err != nil {
 				return fmt.Errorf("reading inventory model yan2d: %w", err)
 			}
 		case 7:
-			def.InventoryModelData.OffsetX, err = reader.ReadUint16()
-			if err != nil {
+			if def.InventoryModelData.OffsetX, err = reader.ReadUint16(); err != nil {
 				return fmt.Errorf("reading inventory model xoffset2d: %w", err)
 			}
 		case 8:
-			def.InventoryModelData.OffsetY, err = reader.ReadUint16()
-			if err != nil {
+			if def.InventoryModelData.OffsetY, err = reader.ReadUint16(); err != nil {
 				return fmt.Errorf("reading inventory model yoffset2d: %w", err)
 			}
 		case 11:
 			def.Stackable = true
 		case 12:
-			def.Value, err = reader.ReadInt32()
-			if err != nil {
+			if def.Value, err = reader.ReadInt32(); err != nil {
 				return fmt.Errorf("reading value: %w", err)
 			}
 		case 13:
-			def.WearPositionPrimary, err = reader.ReadUint8()
-			if err != nil {
+			if def.WearPositionPrimary, err = reader.ReadUint8(); err != nil {
 				return fmt.Errorf("reading wear position primary: %w", err)
 			}
 		case 14:
-			def.WearPositionSecondary, err = reader.ReadUint8()
-			if err != nil {
+			if def.WearPositionSecondary, err = reader.ReadUint8(); err != nil {
 				return fmt.Errorf("reading wear position secondary: %w", err)
 			}
 		case 16:
 			def.MembersOnly = true
 		case 23:
-			def.CharacterModelDataMale.ModelPrimary, err = reader.ReadUint16()
-			if err != nil {
+			if def.CharacterModelDataMale.ModelPrimary, err = reader.ReadUint16(); err != nil {
 				return fmt.Errorf("reading male character model primary: %w", err)
 			}
-			def.CharacterModelDataMale.Offset, err = reader.ReadUint8()
-			if err != nil {
+			if def.CharacterModelDataMale.Offset, err = reader.ReadUint8(); err != nil {
 				return fmt.Errorf("reading male character model offset: %w", err)
 			}
 		case 24:
-			def.CharacterModelDataMale.ModelSecondary, err = reader.ReadUint16()
-			if err != nil {
+			if def.CharacterModelDataMale.ModelSecondary, err = reader.ReadUint16(); err != nil {
 				return fmt.Errorf("reading male character model secondary: %w", err)
 			}
 		case 25:
-			def.CharacterModelDataFemale.ModelPrimary, err = reader.ReadUint16()
-			if err != nil {
+			if def.CharacterModelDataFemale.ModelPrimary, err = reader.ReadUint16(); err != nil {
 				return fmt.Errorf("reading female character model primary: %w", err)
 			}
-			def.CharacterModelDataFemale.Offset, err = reader.ReadUint8()
-			if err != nil {
+			if def.CharacterModelDataFemale.Offset, err = reader.ReadUint8(); err != nil {
 				return fmt.Errorf("reading female character model offset: %w", err)
 			}
 		case 26:
-			def.CharacterModelDataFemale.ModelSecondary, err = reader.ReadUint16()
-			if err != nil {
+			if def.CharacterModelDataFemale.ModelSecondary, err = reader.ReadUint16(); err != nil {
 				return fmt.Errorf("reading female character model secondary: %w", err)
 			}
 		case 27:
-			def.WearPositionTertiary, err = reader.ReadUint8()
-			if err != nil {
+			if def.WearPositionTertiary, err = reader.ReadUint8(); err != nil {
 				return fmt.Errorf("reading wear position tertiary: %w", err)
 			}
 		case 30, 31, 32, 33, 34:
-			def.ActionsGround[opcode-30], err = reader.ReadString()
-			if err != nil {
+			if def.ActionsGround[opcode-30], err = reader.ReadString(); err != nil {
 				return fmt.Errorf("reading ground action 4: %w", err)
 			}
 		case 35, 36, 37, 38, 39:
-			def.ActionsInventory[opcode-35], err = reader.ReadString()
-			if err != nil {
+			if def.ActionsInventory[opcode-35], err = reader.ReadString(); err != nil {
 				return fmt.Errorf("reading inventory action 4: %w", err)
 			}
 		case 40:
@@ -220,12 +200,10 @@ func (def *ItemDefinition) Read(data []byte) error {
 			def.InventoryModelData.RecolorFrom = make([]uint16, length)
 			def.InventoryModelData.RecolorTo = make([]uint16, length)
 			for i := 0; i < int(length); i++ {
-				def.InventoryModelData.RecolorFrom[i], err = reader.ReadUint16()
-				if err != nil {
+				if def.InventoryModelData.RecolorFrom[i], err = reader.ReadUint16(); err != nil {
 					return fmt.Errorf("reading recolor from: %w", err)
 				}
-				def.InventoryModelData.RecolorTo[i], err = reader.ReadUint16()
-				if err != nil {
+				if def.InventoryModelData.RecolorTo[i], err = reader.ReadUint16(); err != nil {
 					return fmt.Errorf("reading recolor to: %w", err)
 				}
 			}
@@ -237,134 +215,108 @@ func (def *ItemDefinition) Read(data []byte) error {
 			def.InventoryModelData.RetextureFrom = make([]uint16, length)
 			def.InventoryModelData.RetextureTo = make([]uint16, length)
 			for i := 0; i < int(length); i++ {
-				def.InventoryModelData.RetextureFrom[i], err = reader.ReadUint16()
-				if err != nil {
+				if def.InventoryModelData.RetextureFrom[i], err = reader.ReadUint16(); err != nil {
 					return fmt.Errorf("reading recolor from: %w", err)
 				}
-				def.InventoryModelData.RetextureTo[i], err = reader.ReadUint16()
-				if err != nil {
+				if def.InventoryModelData.RetextureTo[i], err = reader.ReadUint16(); err != nil {
 					return fmt.Errorf("reading recolor to: %w", err)
 				}
 			}
 		case 42:
-			def.ShiftClickDropIndex, err = reader.ReadUint8()
-			if err != nil {
+			if def.ShiftClickDropIndex, err = reader.ReadUint8(); err != nil {
 				return fmt.Errorf("reading shift click drop index: %w", err)
 			}
 		case 65:
 			def.Exchangeable = true
 		case 75:
-			def.Weight, err = reader.ReadInt16()
-			if err != nil {
+			if def.Weight, err = reader.ReadInt16(); err != nil {
 				return fmt.Errorf("reading weight: %w", err)
 			}
 		case 78:
-			def.CharacterModelDataMale.ModelTertiary, err = reader.ReadUint16()
-			if err != nil {
+			if def.CharacterModelDataMale.ModelTertiary, err = reader.ReadUint16(); err != nil {
 				return fmt.Errorf("reading male character model chat head model secondary: %w", err)
 			}
 		case 79:
-			def.CharacterModelDataFemale.ModelTertiary, err = reader.ReadUint16()
-			if err != nil {
+			if def.CharacterModelDataFemale.ModelTertiary, err = reader.ReadUint16(); err != nil {
 				return fmt.Errorf("reading female character model chat head model secondary: %w", err)
 			}
 		case 90:
-			def.CharacterModelDataMale.ChatHeadModelPrimary, err = reader.ReadUint16()
-			if err != nil {
+			if def.CharacterModelDataMale.ChatHeadModelPrimary, err = reader.ReadUint16(); err != nil {
 				return fmt.Errorf("reading male character model chat head model primary: %w", err)
 			}
 		case 91:
-			def.CharacterModelDataFemale.ChatHeadModelPrimary, err = reader.ReadUint16()
-			if err != nil {
+			if def.CharacterModelDataFemale.ChatHeadModelPrimary, err = reader.ReadUint16(); err != nil {
 				return fmt.Errorf("reading female character model chat head model primary: %w", err)
 			}
 		case 92:
-			def.CharacterModelDataMale.ChatHeadModelSecondary, err = reader.ReadUint16()
-			if err != nil {
+			if def.CharacterModelDataMale.ChatHeadModelSecondary, err = reader.ReadUint16(); err != nil {
 				return fmt.Errorf("reading male character model chat head model secondary: %w", err)
 			}
 		case 93:
-			def.CharacterModelDataFemale.ChatHeadModelSecondary, err = reader.ReadUint16()
-			if err != nil {
+			if def.CharacterModelDataFemale.ChatHeadModelSecondary, err = reader.ReadUint16(); err != nil {
 				return fmt.Errorf("reading female character model chat head model secondary: %w", err)
 			}
 		case 94:
-			def.Category, err = reader.ReadUint16()
-			if err != nil {
+			if def.Category, err = reader.ReadUint16(); err != nil {
 				return fmt.Errorf("reading category: %w", err)
 			}
 		case 95:
-			def.InventoryModelData.RotationZ, err = reader.ReadUint16()
-			if err != nil {
+			if def.InventoryModelData.RotationZ, err = reader.ReadUint16(); err != nil {
 				return fmt.Errorf("reading inventory model rotation z: %w", err)
 			}
 		case 97:
-			def.NotedItemID, err = reader.ReadUint16()
-			if err != nil {
+			if def.NotedItemID, err = reader.ReadUint16(); err != nil {
 				return fmt.Errorf("reading noted item id: %w", err)
 			}
 		case 98:
-			def.NotedTemplate, err = reader.ReadUint16()
-			if err != nil {
+			if def.NotedTemplate, err = reader.ReadUint16(); err != nil {
 				return fmt.Errorf("reading noted template: %w", err)
 			}
 		case 100, 101, 102, 103, 104, 105, 106, 107, 108, 109:
-			def.StackItemIDs[opcode-100], err = reader.ReadUint16()
-			if err != nil {
+			if def.StackItemIDs[opcode-100], err = reader.ReadUint16(); err != nil {
 				return fmt.Errorf("reading stack item ids: %w", err)
 			}
-			def.StackQuantities[opcode-100], err = reader.ReadUint16()
-			if err != nil {
+			if def.StackQuantities[opcode-100], err = reader.ReadUint16(); err != nil {
 				return fmt.Errorf("reading stack quantities: %w", err)
 			}
 		case 110:
-			def.InventoryModelData.ScaleX, err = reader.ReadUint16()
-			if err != nil {
+			if def.InventoryModelData.ScaleX, err = reader.ReadUint16(); err != nil {
 				return fmt.Errorf("reading inventory model scale x: %w", err)
 			}
 		case 111:
-			def.InventoryModelData.ScaleY, err = reader.ReadUint16()
-			if err != nil {
+			if def.InventoryModelData.ScaleY, err = reader.ReadUint16(); err != nil {
 				return fmt.Errorf("reading inventory model scale y: %w", err)
 			}
 		case 112:
-			def.InventoryModelData.ScaleZ, err = reader.ReadUint16()
-			if err != nil {
+			if def.InventoryModelData.ScaleZ, err = reader.ReadUint16(); err != nil {
 				return fmt.Errorf("reading inventory model scale z: %w", err)
 			}
 		case 113:
-			def.InventoryModelData.Ambient, err = reader.ReadInt8()
-			if err != nil {
+			if def.InventoryModelData.Ambient, err = reader.ReadInt8(); err != nil {
 				return fmt.Errorf("reading inventory model ambient: %w", err)
 			}
 		case 114:
-			def.InventoryModelData.Contrast, err = reader.ReadInt8()
-			if err != nil {
+			if def.InventoryModelData.Contrast, err = reader.ReadInt8(); err != nil {
 				return fmt.Errorf("reading inventory model contrast: %w", err)
 			}
 		case 115:
-			def.Team, err = reader.ReadInt8()
-			if err != nil {
+			if def.Team, err = reader.ReadInt8(); err != nil {
 				return fmt.Errorf("reading team: %w", err)
 			}
 		case 139:
-			def.BoughtLinkID, err = reader.ReadUint16()
-			if err != nil {
+			if def.BoughtLinkID, err = reader.ReadUint16(); err != nil {
 				return fmt.Errorf("reading bought link id: %w", err)
 			}
 		case 140:
-			def.BoughtTemplate, err = reader.ReadUint16()
-			if err != nil {
+			if def.BoughtTemplate, err = reader.ReadUint16(); err != nil {
 				return fmt.Errorf("reading bought template: %w", err)
 			}
 		case 148:
-			def.PlaceholderItemID, err = reader.ReadUint16()
-			if err != nil {
+			if def.PlaceholderItemID, err = reader.ReadUint16(); err != nil {
 				return fmt.Errorf("reading placeholder item id: %w", err)
 			}
 		case 149:
-			def.PlaceholderTemplate, err = reader.ReadUint16()
-			if err != nil {
+			if def.PlaceholderTemplate, err = reader.ReadUint16(); err != nil {
 				return fmt.Errorf("reading placeholder template: %w", err)
 			}
 		case 249:
